426b85ac71a43c4fe74982fd116aaab58392653e
Paula Katerinne Campos Campos <pau.campos89@gmail.com>
2018-08-12 14:42:45 -0300

    pruebitas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Calculator} from './calculator';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pruebasunitarias';
+
+  ngOnInit(): void {
+    let calculator = new Calculator();
+    let result = calculator.multiply(3, 3);
+    console.log(result === 9); // 'Test passed'
+    console.log(result !== 12); // 'Test passed'
+  }
 }
---
+++ src/app/calculator.ts
@@ -0,0 +1,5 @@
+export class Calculator {
+    multiply(numberA: number, numberB: number): number {
+        return numberA * numberB;
+    }
+}
